d301cc0ebcc0a84bfd8a6450e390d6861625abb3
Francesco Virga <francescod.virga@gmail.com>
2020-02-03 23:33:40 -0500

    Updatd sql

--- AnimalRescueDatabase.sql
+++ AnimalRescueDatabase.sql
@@ -4,7 +4,7 @@ create table location
 		name varchar(100) not null,
 		address varchar(120) not null,
 		telephone char(11) not null,
-		type enum("spca", "rescue_org", "shelter", "adopter") not null,
+		location_type enum("spca", "rescue_org", "shelter", "adopter") not null,
 		primary key (location_id)
 	);
 
@@ -29,10 +29,10 @@ create table shelter
 		ID varchar(5),
 		location_id varchar(5) not null,
 		website_url varchar(2083) not null,
-		max_dogs numeric(4, 0) check (max_dogs >= 0) not null default 0,
-		max_cats numeric(4, 0) check (max_cats >= 0) not null default 0,
-		max_rabbits numeric(4, 0) check (max_rabbits >= 0) not null default 0,
-		max_rodents numeric(4, 0) check (max_rodents >= 0) not null default 0,
+		max_dogs numeric(4, 0) not null default 0,
+		max_cats numeric(4, 0) not null default 0,
+		max_rabbits numeric(4, 0) not null default 0,
+		max_rodents numeric(4, 0) not null default 0,
 		foreign key (location_id) references location (location_id),
 		primary key (ID)
 	);
@@ -43,16 +43,16 @@ create table adopter
  	 	location_id varchar(5) not null,
  	 	foreign key (location_id) references location (location_id),
  	 	primary key (ID)
-	)
+	);
 
 create table animal
 	(
 		ID varchar(5),
-		type enum('dog', 'cat', 'rabbit', 'rodent') not null
+		animal_type enum("dog", "cat", "rabbit", "rodent") not null,
 		location_id varchar(5) not null,
 		foreign key (location_id) references location (location_id),
 		primary key (ID)
-	)
+	);
 
 create table employee
 	(
@@ -64,7 +64,7 @@ create table employee
 		location_id varchar(5) not null,
 		foreign key (location_id) references location (location_id),
 		primary key (ID)
-	)
+	);
 
 create table driver
 	(
@@ -76,19 +76,19 @@ create table driver
 		rescue_org varchar(5) not null,
  	 	foreign key (rescue_org) references rescue_org (ID),
 		primary key (ID)
-	)
+	);
 
 create table vet_visit
 	(
 		ID varchar(5),
 		name varchar(100) not null,
-		condition varchar(200) not null,
+		animal_condition varchar(200) not null,
 		weight numeric(4,2) not null,
-		date Date not null,
+		visit_date Date not null,
 		animal_id varchar(5) not null,
 		foreign key (animal_id) references animal (ID),
 		primary key (ID)
-	)
+	);
 
 create table trip
 	(
@@ -103,29 +103,29 @@ create table trip
  	 	foreign key (driver_id) references driver (ID),
  	 	foreign key (animal_id) references animal (ID),
 		primary key (ID)
-	)
+	);
 
 create table transaction
 	(
 		ID varchar(5),
-		amount numeric(5, 2) check (amount >= 0) not null,
+		amount numeric(5, 2) not null,
 		animal_id varchar(5) not null,
 		buyer varchar(5) not null,
 		seller varchar(5) not null,
-		date Date not null,
+		transaction_date Date not null,
 		foreign key (animal_id) references animal (ID),
 		foreign key (buyer) references location (location_id),
 		foreign key (seller) references location (location_id),
 		primary key (ID)
-	)
+	);
 
 create table donation
 	(
 		ID varchar(5),
-		amount numeric(5, 2) check (amount >= 0) not null,
+		amount numeric(5, 2) not null,
 		doner_name varchar(100) not null,
 		recipient varchar(5) not null,
-		date Date not null,
+		donation_date Date not null,
 		foreign key (recipient) references location (location_id),
 		primary key (ID)
-	)
+	);
